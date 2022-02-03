efdaf7833d3a1d761ac90d7061e1613b2a2cc78f
Irfan Shah <Irfanshah676@gmail.com>
2022-02-03 10:24:55 +0530

    one chenge

--- src/Components/Parent.jsx
+++ src/Components/Parent.jsx
@@ -4,21 +4,16 @@ import { Button,Input } from 'react-bootstrap';
 import { Search } from './Search';
 import {CounterWithReducer} from "./useReduser/ConterWithReducer"
 export const Parent = () => {
-const [text,setText]= useState("");
-const [text1,setText1]= useState("");
-const [text2,setText2]= useState("");
-const [add,setAdd]= useState("");
-
-
-
-    return (
+const [text,setText]= useState({});
+      
+       return (
         <div>
-            First Name : <input type="text" onChange={(elem)=>{setText(elem.target.value)} } />
-           Meddel Name : <input type="text" onChange={(elem)=>{setText1(elem.target.value)} } />
-           Last Name : <input type="text" onChange={(elem)=>{setText2(elem.target.value)} } />
-           Address : <input type="text" onChange={(elem)=>{setAdd(elem.target.value)} } />
+            First Name : <input type="text" onChange={(elem)=>{{name:setText(elem.target.value)}}} />
+           Meddel Name : <input type="text" onChange={(elem)=>{{Meddel:setText(elem.target.value)}}} />
+           Last Name : <input type="text" onChange={(elem)=>{{Last:setText(elem.target.value)}}} />
+           Address : <input type="text" onChange={(elem)=>{{Address:setText(elem.target.value)}} } />
             
-            <h3 >{ text+" "+text1+" "+text2+" "+add} </h3>
+            <h3 >{ text.name} { text.Meddel} { text.Last}{ text.Address}</h3>
             <Button> Click Me </Button>
 
             <Search/>
