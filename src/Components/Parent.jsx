8cb3172c89b01de9b1d0035cc6f4dfaaeb41ed8a
Irfan Shah <Irfanshah676@gmail.com>
2022-02-13 19:20:35 +0530

    fvjhx

--- src/Components/Parent.jsx
+++ src/Components/Parent.jsx
@@ -10,14 +10,23 @@ const [add,setAdd]= useState("");
       
        return (
         <div>
+            <form>
             <h1>PARANT COM</h1>
             First Name : <input type="text" onChange={(elem)=>{{setName(elem.target.value)}}} />
            Meddel Name : <input type="text" onChange={(elem)=>{{setMname(elem.target.value)}}} />
            Last Name : <input type="text" onChange={(elem)=>{{setLname(elem.target.value)}}} />
            Address : <input type="text" onChange={(elem)=>{{setAdd(elem.target.value)}} } />
-            
-            <h3 >{ name} { mname} {lname} {add}</h3>
-            <Button> Click Me </Button>
+    
+             <div>
+            <h3 > Name         :{uppercase,name} <br />
+                  Meddel Name  :{mname} <br />
+                  Last Name    :{lname} <br />
+                  Address      :{add}</h3>
+
+            <input type="reset" value="Reset" />
+            <input type="submit" value="Submit" />
+            </div>
+            </form>
             
 
 
